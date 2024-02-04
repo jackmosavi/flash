cfced174a178591ebf802f7cae3797ea3dc18eee
Jacki <145014864+jackmosavi@users.noreply.github.com>
2024-02-04 11:05:18 +0000

    Update modal.blade.php

--- src/views/modal.blade.php
+++ src/views/modal.blade.php
@@ -10,7 +10,9 @@
             <div class="modal-body">
                 <p>{!! $body !!}</p>
             </div>
-
+            <div>
+                <p>Hi bro loading ...</p>
+            </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
